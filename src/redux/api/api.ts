24d8ef8f186ece302dc3239b95b4bbd6a6ff4f0f
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2024-07-03 23:27:15 +0600

    add: movies get and post redux api

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -2,6 +2,22 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "BACKEND API URL" }),
-  endpoints: () => ({}),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  endpoints: (builder) => ({
+    getMovies: builder.query({
+      query: () => ({
+        url: "/movies",
+        method: "GET",
+      }),
+    }),
+    addMovies: builder.mutation({
+      query: (data) => ({
+        url: "/movies",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
 });
+
+export const { useGetMoviesQuery, useAddMoviesMutation } = baseApi;
