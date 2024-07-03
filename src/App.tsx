647f5143c39e95857b7f736dfc80dd1973ce9f50
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2024-07-03 13:28:09 +0600

    add: carousel banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
+import { HeroSection } from "./components/HeroSection/HeroSection";
 import { MovieCard } from "./components/MovieCard/MovieCard";
-import { Button } from "./components/ui/button";
-import {
-  decrement,
-  increment,
-  incrementByValue,
-} from "./redux/features/counter/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hooks";
 
 const App = () => {
-  const dispatch = useAppDispatch();
-  const count = useAppSelector((state) => state.counter.value);
-
   return (
     <div>
-      <div className="flex h-screen w-full justify-center items-center ">
-        <div className="flex items-center border border-sky-500 rounded-md p-12 gap-2">
-          <Button
-            className="bg-green-500"
-            onClick={() => dispatch(increment())}
-          >
-            Increment
-          </Button>
-          <Button
-            className="bg-blue-500"
-            onClick={() => dispatch(incrementByValue(5))}
-          >
-            Increment By 5
-          </Button>
-          <h1 className="mx-10">{count}</h1>
-          <Button className="bg-red-500" onClick={() => dispatch(decrement())}>
-            Decrement
-          </Button>
-        </div>
-      </div>
-      <div className="w-full flex justify-center my-5">
+      <HeroSection />
+      <div className="grid md:grid-cols-4 sm:grid-cols-1 gap-2 w-[90%] mx-auto my-5">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
         <MovieCard />
       </div>
     </div>
---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,54 @@
+import { Card, CardContent } from "@/components/ui/card";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import sliderImage1 from "@/assets/avengers.jpg";
+import sliderImage2 from "@/assets/justice-league-superman-banner.png";
+import sliderImage3 from "@/assets/doctor-strange.jpg";
+
+export function HeroSection() {
+  const sliderData = [
+    {
+      id: 1,
+      image: sliderImage1,
+    },
+    {
+      id: 2,
+      image: sliderImage2,
+    },
+    {
+      id: 3,
+      image: sliderImage3,
+    },
+  ];
+
+  return (
+    <Carousel
+      className="w-[90%] mx-auto my-5"
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {sliderData.map((slider) => (
+          <CarouselItem key={slider.id}>
+            <Card>
+              <CardContent className="flex items-center justify-center h-[600px] p-1">
+                <img src={slider?.image} className="h-full w-full" alt="" />
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
