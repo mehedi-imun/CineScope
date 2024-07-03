225b270355be8b932ab43179482824bb5e632e6f
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2024-07-03 12:51:22 +0600

    add: card with info

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,5 +1,4 @@
 import { Star, Plus } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,20 +9,24 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import movieImage from "@/assets/inception.png";
+
 type CardProps = React.ComponentProps<typeof Card>;
 
 export function MovieCard({ className, ...props }: CardProps) {
   return (
     <Card className={cn("w-[380px]", className)} {...props}>
       <CardHeader>
-        <div className="flex  items-center">
-          <Star />
-          <p>8</p>
+        <img src={movieImage} className="max-h-[500px]" alt="" />
+        <div className="flex  items-center gap-2">
+          <Star color="orange" fill="orange" />
+          <p className="text-2xl">8.7</p>
         </div>
-
         <CardTitle>Inception</CardTitle>
       </CardHeader>
-      <CardContent className="grid gap-4"></CardContent>
+      <CardContent className="grid">
+        <p className="text-xl">Sci-Fi</p>
+      </CardContent>
       <CardFooter>
         <Button className="w-full">
           <Plus className="mr-2 h-4 w-4" />
