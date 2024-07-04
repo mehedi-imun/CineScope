924432da61e5d2253c5c44b51ca8f46e94f6d0ef
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2024-07-04 23:02:44 +0600

    add: modal input fields

--- src/components/RatingModal/RatingMdal.tsx
+++ src/components/RatingModal/RatingMdal.tsx
@@ -23,35 +23,25 @@ export function RatingModal() {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
+          <DialogTitle>RATE THIS</DialogTitle>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
+          <Input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            className="col-span-3"
+          />
+          <Input
+            id="comment"
+            placeholder="Enter your comment"
+            className="col-span-3"
+          />
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit" className="w-full">
+            Rate
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
