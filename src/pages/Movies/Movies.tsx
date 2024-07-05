841eead867721b8c00bbcb23ff8f3e2f7662471d
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2024-07-06 00:14:57 +0600

    add: all movie data added

--- src/pages/Movies/Movies.tsx
+++ src/pages/Movies/Movies.tsx
@@ -1,13 +1,22 @@
 import { MovieCard } from "@/components/MovieCard/MovieCard";
+import { useGetMoviesQuery } from "@/redux/api/api";
+import { TMovie } from "@/types";
 
-export default function movies() {
+export default function Movies() {
+  const { data: movies, isLoading } = useGetMoviesQuery({});
+  // console.log(movies);
+  if (isLoading)
+    return (
+      <p className="text-3xl text-center text-yellow-500 my-2 font-bold">
+        Loading....
+      </p>
+    );
   return (
     <div className="container mx-auto">
       <div className="grid md:grid-cols-4 sm:grid-cols-1 gap-4 mx-auto my-5">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        {movies?.data?.map((movie: TMovie) => (
+          <MovieCard key={movie?._id} movie={movie} />
+        ))}
       </div>
     </div>
   );
