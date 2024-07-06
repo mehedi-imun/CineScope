47d5d3b7ca1631050c055fa13d441b55a32715ff
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2024-07-06 11:11:19 +0600

    add: api tags for caching

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -3,32 +3,30 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  tagTypes: ["movies"],
   endpoints: (builder) => ({
     getMovies: builder.query({
       query: () => ({
         url: "/movies",
         method: "GET",
       }),
+      providesTags: ["movies"],
     }),
     getSingleMovie: builder.query({
       query: (slug) => ({
         url: `/movies/${slug}`,
         method: "GET",
       }),
+      providesTags: ["movies"],
     }),
 
     addRating: builder.mutation({
-      query: ({ data, slug }) => {
-        console.log(data, slug);
-        return {
-          url: `/movies/${slug}/review`,
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: data,
-        };
-      },
+      query: ({ data, slug }) => ({
+        url: `/movies/${slug}/review`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["movies"],
     }),
   }),
 });
